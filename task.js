e5d558ab257de2773b15008b7920510dc7d0cebe
Alex5453 <130847438+Alex5453@users.noreply.github.com>
2025-04-07 19:52:00 +0300

    Update task.js

--- task.js
+++ task.js
@@ -1,7 +1,6 @@
 "use strict";
 
 // Задача 1. Форматтер чисел
-
 function parseCount(value) {
   const result = Number.parseFloat(value);
   if (Number.isNaN(result)) {
@@ -19,14 +18,9 @@ function validateCount(value) {
 }
 
 // Задача 2. Класс Triangle
-
 class Triangle {
   constructor(a, b, c) {
-    if (
-      a + b <= c ||
-      a + c <= b ||
-      b + c <= a
-    ) {
+    if (a + b <= c || a + c <= b || b + c <= a) {
       throw new Error("Треугольник с такими сторонами не существует");
     }
     this.a = a;
@@ -40,9 +34,7 @@ class Triangle {
 
   get area() {
     const p = this.perimeter / 2;
-    const s = Math.sqrt(
-      p * (p - this.a) * (p - this.b) * (p - this.c)
-    );
+    const s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
     return +s.toFixed(3);
   }
 }
@@ -61,19 +53,3 @@ function getTriangle(a, b, c) {
     };
   }
 }
-
-
-//тесты
-function testCase() {
-    console.log(validateCount("123.45")); // 123.45
-    console.log(validateCount("abc")); // Error
-  
-    const validTriangle = getTriangle(3, 4, 5);
-    console.log(validTriangle.perimeter); // 12
-    console.log(validTriangle.area); // 6.000
-  
-    const invalidTriangle = getTriangle(1, 1, 100);
-    console.log(invalidTriangle.perimeter); // "Ошибка! Треугольник не существует"
-    console.log(invalidTriangle.area);      // "Ошибка! Треугольник не существует"
-  }
-  
